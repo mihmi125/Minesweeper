b4d9552d79df51d41d7426d9a125a3beedbcdc18
Mihkel Ellik <mihkelellik@gmail.com>
2026-05-14 11:02:53 +0300

    Some bug fixes

--- logic/board.py
+++ logic/board.py
@@ -1,4 +1,4 @@
-from cell import Cell
+from logic.cell import Cell
 from random import randint
 
 class Board:
@@ -20,7 +20,7 @@ class Board:
             c = randint(0, self.columns - 1)
 
             if not self.grid[r][c].is_mine:
-                self.grid[r][c].mine = True
+                self.grid[r][c].is_mine = True
                 mines_placed += 1
 
     def _get_neighbors(self, row, col):
@@ -29,11 +29,12 @@ class Board:
             for c in range(col - 1, col + 2):
                 if (0 <= r < self.rows and 0 <= c < self.columns) and (r != row or c != col):
                     neighbors.append((r, c))
+        return neighbors
 
     def _calculate_neighbors(self):
         for r in range(0, self.rows):
             for c in range(0, self.columns):
                 if not self.grid[r][c].is_mine:
                     neighbor = self._get_neighbors(r, c)
-                    mine_count = sum(1 for nr, nc in neighbor if self.grid[r][c].is_mine)
-                    self.grid[r][c].neighbors_mines = mine_count
+                    mine_count = sum(1 for nr, nc in neighbor if self.grid[nr][nc].is_mine)
+                    self.grid[r][c].neighbor_mines = mine_count
