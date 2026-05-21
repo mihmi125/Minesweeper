bdca4e7d795d5bb858b4704e9a7922c283873d8e
Mihkel <mihkelellik@gmail.com>
2026-05-21 13:20:34 +0300

    made game.py and added main.py to start the game

--- core/game.py
+++ core/game.py
@@ -2,6 +2,7 @@ import config
 from logic.board import Board
 from ui.interface import Interface
 
+
 class Game:
     def __init__(self):
         self.board = Board(
@@ -10,13 +11,20 @@ class Game:
             mine_count=config.MINE_COUNT
         )
 
-        self.ui = Interface(
-            width=config.SCR_WIDTH,
-            height=config.SCR_HEIGHT,
-            title=config.TITLE,
-            rows=config.GRID_ROWS,
-            cols=config.GRID_COLS,
-        )
+        self.ui = Interface(on_click_callback=self.handle_cell_click)
+        self.ui.set_board(self.board)
 
-        self.ui.cell_size = config.TILE_SIZE
-        self.sync_ui_with_board()
+    def handle_cell_click(self, row, col, button):
+        if button == 1:
+            continue_game = self.board.reveal_cell(row, col)
+
+            if not continue_game:
+                print("GAME OVER")
+            elif self.board.check_win():
+                print("YOU WIN!")
+
+        elif button == 3:
+            self.board.toggle_flag(row, col)
+
+    def run(self):
+        self.ui.run()
